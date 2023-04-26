b138c8483e8e258cb54a9726af03f8bd1499bf16
FarahAlhalimi <engfarahhalimy@gmail.com>
2023-04-26 09:13:38 +0300

    deployment

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -7,9 +7,9 @@ let DATABASE_URL = '';
 const node_env = process.env.NODE_ENV;
 
 if (node_env === 'production') {
-  DATABASE_URL = process.env.DEPLOY_DB_URL;
+  DATABASE_URL = 'postgres://myreddit:fnkv9eyPn2s6iN5N8KXrCLw87fmDLWqt@dpg-ch4a469n852hpi1ofneg-a.frankfurt-postgres.render.com/myreddit';
 } else if (node_env === 'development') {
-  DATABASE_URL = 'postgres://myreddit:fnkv9eyPn2s6iN5N8KXrCLw87fmDLWqt@dpg-ch4a469n852hpi1ofneg-a.frankfurt-postgres.render.com/myreddit'
+  DATABASE_URL = process.env.DB_URL;
 } else {
   throw new Error('DB_URL NOT FOUND!');
 }
