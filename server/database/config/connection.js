21f086f849b7b1cdc41d70525ade17ae9804c296
FarahAlhalimi <engfarahhalimy@gmail.com>
2023-04-26 09:12:55 +0300

    deployment

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -9,7 +9,7 @@ const node_env = process.env.NODE_ENV;
 if (node_env === 'production') {
   DATABASE_URL = process.env.DEPLOY_DB_URL;
 } else if (node_env === 'development') {
-  DATABASE_URL = process.env.DB_URL;
+  DATABASE_URL = 'postgres://myreddit:fnkv9eyPn2s6iN5N8KXrCLw87fmDLWqt@dpg-ch4a469n852hpi1ofneg-a.frankfurt-postgres.render.com/myreddit'
 } else {
   throw new Error('DB_URL NOT FOUND!');
 }
